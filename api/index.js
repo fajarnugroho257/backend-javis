cbcc3f7070e9a81b8caeea7b15b60769a2d277cc
fajarnugroho257 <fajarnugroho274@gmail.com>
2025-10-23 05:48:58 +0700

    add api/index.js

---
+++ api/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const { connectDB } = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+require("dotenv").config();
+const cors = require("cors");
+
+const app = express();
+
+// CORS
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Sesuaikan dengan alamat frontend
+    credentials: true,
+  })
+);
+
+app.use(express.urlencoded({ extended: true }));
+
+const port = 8000;
+
+connectDB();
+app.use(express.json());
+
+// body
+app.use((req, res, next) => {
+  console.log("Body diterima:", req.body);
+  next();
+});
+
+app.use("/auth", authRoutes);
+
+module.exports = app;
